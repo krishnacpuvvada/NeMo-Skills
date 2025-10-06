d2010ad6a9405bb7ed84adb0d376cc34c1785d4d
Shantanu Acharya <shantanua@nvidia.com>
2025-10-06 14:36:17 -0400

    Add option to disable auto running of `summarize_results` (#895)
    Signed-off-by: Shantanu Acharya <shantanua@nvidia.com>
    Co-authored-by: George Armstrong <georgea@nvidia.com>

--- nemo_skills/pipeline/eval.py
+++ nemo_skills/pipeline/eval.py
@@ -132,6 +132,9 @@ def eval(
     time_min: str = typer.Option(None, help="If specified, will use as a time-min slurm parameter"),
     mount_paths: str = typer.Option(None, help="Comma separated list of paths to mount on the remote machine"),
     extra_eval_args: str = typer.Option("", help="Additional arguments for evaluation"),
+    auto_summarize_results: bool = typer.Option(
+        True, help="If True, will automatically launch summarize results tasks"
+    ),
     single_node_mode: SingleNodeMode = typer.Option(
         SingleNodeMode.parallel,
         help="Whether to run benchmarks in parallel or sequentially on a single node. "
@@ -441,86 +444,87 @@ def eval(
         group_module = {}
 
         # setting summarize results tasks
-        for benchmark, benchmark_args in benchmarks_dict.items():
-            # TODO: add logic if metrics.json exists, we don't run this!
-            has_tasks = True
-            metric_file = f"{output_dir}/{benchmark_args.eval_subfolder}/metrics.json"
-            # TODO: with this new usage summarize_results probably needs some refactoring
-            #       also maybe we should remove it from pipeline as it's not
-            #       really ever needed to be run directly anymore?
-            results_folder = f"{output_dir}/{Path(benchmark_args.eval_subfolder).parent}"
-            command = (
-                f"python -m nemo_skills.pipeline.summarize_results {results_folder} "
-                f"    --benchmarks {benchmark} "
-                f"    --save_metrics_path {metric_file} "
-            )
-            if wandb_name:
-                command += f" --wandb_name={wandb_name} "
-            if wandb_group:
-                command += f" --wandb_group={wandb_group} "
-            if wandb_project:
-                command += f" --wandb_project={wandb_project} "
-            if data_dir:
-                command += f" --data_dir={data_dir} "
+        if auto_summarize_results:
+            for benchmark, benchmark_args in benchmarks_dict.items():
+                # TODO: add logic if metrics.json exists, we don't run this!
+                has_tasks = True
+                metric_file = f"{output_dir}/{benchmark_args.eval_subfolder}/metrics.json"
+                # TODO: with this new usage summarize_results probably needs some refactoring
+                #       also maybe we should remove it from pipeline as it's not
+                #       really ever needed to be run directly anymore?
+                results_folder = f"{output_dir}/{Path(benchmark_args.eval_subfolder).parent}"
+                command = (
+                    f"python -m nemo_skills.pipeline.summarize_results {results_folder} "
+                    f"    --benchmarks {benchmark} "
+                    f"    --save_metrics_path {metric_file} "
+                )
+                if wandb_name:
+                    command += f" --wandb_name={wandb_name} "
+                if wandb_group:
+                    command += f" --wandb_group={wandb_group} "
+                if wandb_project:
+                    command += f" --wandb_project={wandb_project} "
+                if data_dir:
+                    command += f" --data_dir={data_dir} "
 
-            if benchmark in benchmark_to_judge_tasks:
-                dependent_tasks = benchmark_to_judge_tasks[benchmark]
-            else:
-                dependent_job_ids = benchmark_args.job_ids
-                dependent_tasks = []
-                for job_id in dependent_job_ids:
-                    dependent_tasks.extend(job_id_to_tasks[job_id])
+                if benchmark in benchmark_to_judge_tasks:
+                    dependent_tasks = benchmark_to_judge_tasks[benchmark]
+                else:
+                    dependent_job_ids = benchmark_args.job_ids
+                    dependent_tasks = []
+                    for job_id in dependent_job_ids:
+                        dependent_tasks.extend(job_id_to_tasks[job_id])
 
-            summarize_task = pipeline_utils.add_task(
-                exp,
-                cmd=command,
-                task_name=f"{expname}-{benchmark}-summarize-results",
-                log_dir=f"{output_dir}/{benchmark_args.eval_subfolder}/summarized-results",
-                container=cluster_config["containers"]["nemo-skills"],
-                cluster_config=cluster_config,
-                run_after=run_after,
-                reuse_code_exp=reuse_code_exp,
-                reuse_code=reuse_code,
-                task_dependencies=(
-                    dependent_tasks if cluster_config["executor"] == "slurm" else all_tasks + _task_dependencies
-                ),
-                installation_command=installation_command,
-                skip_hf_home_check=skip_hf_home_check,
-            )
-            all_tasks.append(summarize_task)
-            if benchmark_args.benchmark_group:
-                group_metric_files[benchmark_args.benchmark_group].append(metric_file)
-                group_tasks[benchmark_args.benchmark_group].append(summarize_task)
-                # it's always the same for all benchmarks in a group
-                group_module[benchmark_args.benchmark_group] = benchmark_args.score_module
+                summarize_task = pipeline_utils.add_task(
+                    exp,
+                    cmd=command,
+                    task_name=f"{expname}-{benchmark}-summarize-results",
+                    log_dir=f"{output_dir}/{benchmark_args.eval_subfolder}/summarized-results",
+                    container=cluster_config["containers"]["nemo-skills"],
+                    cluster_config=cluster_config,
+                    run_after=run_after,
+                    reuse_code_exp=reuse_code_exp,
+                    reuse_code=reuse_code,
+                    task_dependencies=(
+                        dependent_tasks if cluster_config["executor"] == "slurm" else all_tasks + _task_dependencies
+                    ),
+                    installation_command=installation_command,
+                    skip_hf_home_check=skip_hf_home_check,
+                )
+                all_tasks.append(summarize_task)
+                if benchmark_args.benchmark_group:
+                    group_metric_files[benchmark_args.benchmark_group].append(metric_file)
+                    group_tasks[benchmark_args.benchmark_group].append(summarize_task)
+                    # it's always the same for all benchmarks in a group
+                    group_module[benchmark_args.benchmark_group] = benchmark_args.score_module
 
-        # if we have any benchmark groups, submitting final aggregation for those
-        # TODO: this should be done by summarize_results directly and we just call it on a group
-        #       otherwise behavior is inconsistent when running summarize_results standalone, which isn't great
-        for group, metric_files in group_metric_files.items():
-            has_tasks = True
-            command = (
-                f"python -m nemo_skills.evaluation.compute_group_score {' '.join(metric_files)} "
-                f"    --score_module {group_module[group]} "
-                f"    --save_metrics_file {output_dir}/eval-results/{group}/metrics.json "
-            )
-            score_task = pipeline_utils.add_task(
-                exp,
-                cmd=command,
-                task_name=f"{expname}-{group}-compute-score",
-                log_dir=f"{output_dir}/eval-results/{group}/compute-score-logs",
-                container=cluster_config["containers"]["nemo-skills"],
-                cluster_config=cluster_config,
-                run_after=run_after,
-                reuse_code_exp=reuse_code_exp,
-                reuse_code=reuse_code,
-                task_dependencies=(
-                    group_tasks[group] if cluster_config["executor"] == "slurm" else all_tasks + _task_dependencies
-                ),
-                installation_command=installation_command,
-                skip_hf_home_check=skip_hf_home_check,
-            )
-            all_tasks.append(score_task)
+            # if we have any benchmark groups, submitting final aggregation for those
+            # TODO: this should be done by summarize_results directly and we just call it on a group
+            #       otherwise behavior is inconsistent when running summarize_results standalone, which isn't great
+            for group, metric_files in group_metric_files.items():
+                has_tasks = True
+                command = (
+                    f"python -m nemo_skills.evaluation.compute_group_score {' '.join(metric_files)} "
+                    f"    --score_module {group_module[group]} "
+                    f"    --save_metrics_file {output_dir}/eval-results/{group}/metrics.json "
+                )
+                score_task = pipeline_utils.add_task(
+                    exp,
+                    cmd=command,
+                    task_name=f"{expname}-{group}-compute-score",
+                    log_dir=f"{output_dir}/eval-results/{group}/compute-score-logs",
+                    container=cluster_config["containers"]["nemo-skills"],
+                    cluster_config=cluster_config,
+                    run_after=run_after,
+                    reuse_code_exp=reuse_code_exp,
+                    reuse_code=reuse_code,
+                    task_dependencies=(
+                        group_tasks[group] if cluster_config["executor"] == "slurm" else all_tasks + _task_dependencies
+                    ),
+                    installation_command=installation_command,
+                    skip_hf_home_check=skip_hf_home_check,
+                )
+                all_tasks.append(score_task)
 
         if has_tasks:
             pipeline_utils.run_exp(exp, cluster_config, dry_run=dry_run)
